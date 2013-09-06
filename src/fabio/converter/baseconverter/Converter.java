f1e030f3ae373ca3417f3345f1def2297b8d9612
Fabio Teixeira + Andre Bernades <fabio1079@gmail.com>
2013-09-06 15:28:07 -0300

    Convert decimal to binary and binary to decimal

--- src/fabio/converter/baseconverter/Converter.java
+++ src/fabio/converter/baseconverter/Converter.java
@@ -2,4 +2,38 @@ package fabio.converter.baseconverter;
 
 public class Converter {
 	
+	public int decimalToBinary(int number) {
+		String result = new String();
+		
+		while( number > 0 ) {
+			result += Integer.toString(number%2);
+			number /=  2;
+		}
+		
+		return Integer.valueOf(this.reverseString(result));
+	}
+	
+	public int binaryToDecimal(int number) {
+		String value = new String(Integer.toString(number));
+		value = this.reverseString(value);
+		
+		int result = 0;
+		
+		for(int i = 0; i < value.length(); i++) {
+			System.out.println(Math.pow(2, i));
+			result += Character.getNumericValue(value.charAt(i)) * Math.pow(2, i);
+		}
+		
+		return result;
+	}
+	
+	
+	private String reverseString(String value) {
+		String result = new String();
+		
+		for(int i = value.length()-1; i >= 0; i--)
+			result += value.charAt(i);
+		
+		return result;
+	}
 }
