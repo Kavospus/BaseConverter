4215f1d6610f2a3f743e618cc13bdfdf9a2ec95c
Fabio Teixeia <fabio1079@gmail.com>
2013-09-07 12:51:40 -0300

    Now the class Converter can convert decimal to binary, octal and hex

--- src/fabio/converter/baseconverter/Converter.java
+++ src/fabio/converter/baseconverter/Converter.java
@@ -2,15 +2,26 @@ package fabio.converter.baseconverter;
 
 public class Converter {
 	
-	public int decimalToBinary(int number) {
+	public String decimalToBinary(int number) {
+		return this.decimalToBaseN(number, 2);
+	}
+	
+	public String decimalToOctal(int number) {
+		return this.decimalToBaseN(number, 8);
+	}
+	
+	public String decimalToHex(int number) {
 		String result = new String();
+		int remainder;
 		
-		while( number > 0 ) {
-			result += Integer.toString(number%2);
-			number /=  2;
+		while(number > 0) {
+			remainder = number%16;
+			
+			result += this.numberValueInHex(remainder);
+			number /=  16;
 		}
 		
-		return Integer.valueOf(this.reverseString(result));
+		return this.reverseString(result);
 	}
 	
 	public int binaryToDecimal(int number) {
@@ -20,7 +31,6 @@ public class Converter {
 		int result = 0;
 		
 		for(int i = 0; i < value.length(); i++) {
-			System.out.println(Math.pow(2, i));
 			result += Character.getNumericValue(value.charAt(i)) * Math.pow(2, i);
 		}
 		
@@ -28,11 +38,46 @@ public class Converter {
 	}
 	
 	
+	
 	private String reverseString(String value) {
 		String result = new String();
 		
 		for(int i = value.length()-1; i >= 0; i--)
 			result += value.charAt(i);
+		
+		return result;
+	}
+	
+	private String decimalToBaseN(int decimalNumber, int base) {
+		if(base < 2)
+			return "-1";
+		
+		String result = new String();
+		
+		while(decimalNumber > 0) {
+			result += Integer.toString(decimalNumber%base);
+			decimalNumber /=  base;
+		}
+		
+		return this.reverseString(result);
+	}
+	
+	private String numberValueInHex(int number) {
+		String result = "";
+		
+		if(number >= 0 && number <= 9)
+			result = Integer.toString(number);
+		else if(number > 15)
+			result = "Er";
+		else
+			switch(number) {
+				case 10: result = "A"; break;
+				case 11: result = "B"; break;
+				case 12: result = "C"; break;
+				case 13: result = "D"; break;
+				case 14: result = "E"; break;
+				case 15: result = "F"; break;
+			}
 		
 		return result;
 	}
