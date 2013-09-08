ee74d1469a4015a93f7c55e587abb693b6867b79
André Bernardes <andrebsguedes@gmail.com>
2013-09-08 14:41:52 -0300

    Tweaks on Converter and Event Handling done

--- src/fabio/converter/baseconverter/Converter.java
+++ src/fabio/converter/baseconverter/Converter.java
@@ -1,141 +1,123 @@
 package fabio.converter.baseconverter;
 
 public class Converter {
-	
-	// Convert decimal to binary
-	public long decimalToBinary(long number) {
-		if(number < 2)
-			return number;
-		else
-			return this.decimalToBaseN(number, 2);
-	}
-	
-	// Convert decimal to octal
-	public long decimalToOctal(long number) {
-		if(number < 8)
-			return number;
-		else
-			return this.decimalToBaseN(number, 8);
-	}
-	
-	// Convert decimal to hexadecimal
-	public String decimalToHex(long number) {
-		String result = new String();
-		int remainder;
-		
-		if( number < 9 )
-			return Long.toString(number);
-		else {
-			while(number > 0) {
-				remainder = (int)(number%16);
-			
-				result += this.numberValueInHex(remainder);
-				number /=  16;
-			}
-		
-			return this.reverseString(result);
-		}
-	}
-	
-	// Convert binary to decimal
-	public long binaryToDecimal(long number) {
-		return this.baseNToDecimal(number, 2);
-	}
-	
-	// Convert octal to decimal
-	public long octalToDecimal(long number) {
-		return this.baseNToDecimal(number, 8);
-	}
-	
-	// Convert hexadecimal to decimal
-	public long hexToDecimal(String value) {
-		value = this.reverseString(value);
-		long result = 0;
-		int number;
-		
-		for(int i = 0; i < value.length(); i++) {
-			number = this.hexCharValueInNumber(value.charAt(i));
-			result += number * Math.pow(16, i);
-		}
-		
-		return result;
-	}
-	
-	
-	// Reverse a string
-	private String reverseString(String value) {
-		String result = new String();
-		
-		for(int i = value.length()-1; i >= 0; i--)
-			result += value.charAt(i);
-		
-		return result;
-	}
-	
-	// Convert N base to decimal
-	private long baseNToDecimal(long number, int base) {
-		String value = new String(Long.toString(number));
-		value = this.reverseString(value);
-		
-		long result = 0;
-		
-		for(int i = 0; i < value.length(); i++) {
-			result += Character.getNumericValue(value.charAt(i)) * Math.pow(base, i);
-		}
-		
-		return result;
-	}
-	
-	// Convert decimal to N base up to 10
-	private int decimalToBaseN(long decimalNumber, int base) {
-		String result = new String();
-		
-		while(decimalNumber > 0) {
-			result += Long.toString(decimalNumber%base);
-			decimalNumber /=  base;
-		}
-		result = this.reverseString(result);
-		
-		return Integer.valueOf(result);
-	}
-	
-	// Get the hexadecimal of a number
-	private String numberValueInHex(int number) {
-		String result = "";
-		
-		if(number >= 0 && number <= 9)
-			result = Integer.toString(number);
-		else if(number > 15)
-			result = "Er";
-		else
-			switch(number) {
-				case 10: result = "A"; break;
-				case 11: result = "B"; break;
-				case 12: result = "C"; break;
-				case 13: result = "D"; break;
-				case 14: result = "E"; break;
-				case 15: result = "F"; break;
-			}
-		
-		return result;
-	}
-	
-	// Get the number value of a hexadecimal character value
-	private int hexCharValueInNumber(char value) {
-		int result = 0;
-		
-		if( Character.isDigit(value) )
-			result = Character.getNumericValue(value);
-		else
-			switch(value) {
-				case 'A': result = 10; break;
-				case 'B': result = 11; break;
-				case 'C': result = 12; break;
-				case 'D': result = 13; break;
-				case 'E': result = 14; break;
-				case 'F': result = 15; break;
-			}
-		
-		return result;
-	}
+
+    // Convert decimal to binary
+    public String decimalToBinary(long number) {
+        return Long.toBinaryString(number);
+    }
+
+    // Convert decimal to octal
+    public String decimalToOctal(long number) {
+        return Long.toOctalString(number);
+    }
+
+    // Convert decimal to hexadecimal
+    public String decimalToHex(long number) {
+        return Long.toHexString(number);
+
+    }
+
+
+    // Convert binary to decimal
+    public long binaryToDecimal(long number) {
+        return this.baseNToDecimal(number, 2);
+    }
+
+    // Convert octal to decimal
+    public long octalToDecimal(long number) {
+        return this.baseNToDecimal(number, 8);
+    }
+
+    // Convert hexadecimal to decimal
+    public long hexToDecimal(String value) {
+        value = this.reverseString(value);
+        long result = 0;
+        int number;
+
+        for(int i = 0; i < value.length(); i++) {
+            number = this.hexCharValueInNumber(value.charAt(i));
+            result += number * Math.pow(16, i);
+        }
+
+        return result;
+    }
+
+
+    // Reverse a string
+    private String reverseString(String value) {
+        String result = new String();
+
+        for(int i = value.length()-1; i >= 0; i--)
+            result += value.charAt(i);
+
+        return result;
+    }
+
+    // Convert N base to decimal
+    private long baseNToDecimal(long number, int base) {
+        String value = new String(Long.toString(number));
+        value = this.reverseString(value);
+
+        long result = 0;
+
+        for(int i = 0; i < value.length(); i++) {
+            result += Character.getNumericValue(value.charAt(i)) * Math.pow(base, i);
+        }
+
+        return result;
+    }
+
+    // Convert decimal to N base up to 10
+    private int decimalToBaseN(long decimalNumber, int base) {
+        String result = new String();
+
+        while(decimalNumber > 0) {
+            result += Long.toString(decimalNumber%base);
+            decimalNumber /=  base;
+        }
+        result = this.reverseString(result);
+
+        return Integer.valueOf(result);
+    }
+
+    // Get the hexadecimal of a number
+    private String numberValueInHex(int number) {
+        String result = "";
+
+        if(number >= 0 && number <= 9)
+            result = Integer.toString(number);
+        else if(number > 15)
+            result = "Er";
+        else
+            switch(number) {
+                case 10: result = "A"; break;
+                case 11: result = "B"; break;
+                case 12: result = "C"; break;
+                case 13: result = "D"; break;
+                case 14: result = "E"; break;
+                case 15: result = "F"; break;
+            }
+
+        return result;
+    }
+
+    // Get the number value of a hexadecimal character value
+    private int hexCharValueInNumber(char value) {
+        int result = 0;
+
+        if( Character.isDigit(value) )
+            result = Character.getNumericValue(value);
+        else
+            switch(value) {
+                case 'A': result = 10; break;
+                case 'B': result = 11; break;
+                case 'C': result = 12; break;
+                case 'D': result = 13; break;
+                case 'E': result = 14; break;
+                case 'F': result = 15; break;
+            }
+
+        return result;
+    }
 }
--- src/fabio/converter/baseconverter/MainActivity.java
+++ src/fabio/converter/baseconverter/MainActivity.java
@@ -3,20 +3,112 @@ package fabio.converter.baseconverter;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+        final TextView result = (TextView) findViewById(R.id.textViewResult);
+        final TextView data = (TextView) findViewById(R.id.editTextValue);
+        final RadioGroup baseFrom = (RadioGroup) findViewById(R.id.radioGroup1);
+        final RadioGroup baseTo = (RadioGroup) findViewById(R.id.radioGroup2);
+
+        final Button button = (Button) findViewById(R.id.buttonConvert);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                result.setText(conversionCaller(data.getText().toString(),baseFrom.getCheckedRadioButtonId(),baseTo.getCheckedRadioButtonId()));
+            }
+        });
+
+    }
+
+    public String conversionCaller(String value,int from, int to){
+        String printResult ="";
+        Converter convert = new Converter();
+        switch (to){
+            case R.id.radioToBase2:
+                switch (from){
+                    case R.id.radioFromBase2:
+                        printResult = value;
+                        break;
+                    case R.id.radioFromBase8:
+                        printResult = convert.decimalToBinary(convert.octalToDecimal(Long.parseLong(value)));
+                        break;
+                    case R.id.radioFromBase10:
+                        printResult = convert.decimalToBinary(Long.parseLong(value));
+                        break;
+                    case R.id.radioFromBase16:
+                        printResult = convert.decimalToBinary(convert.hexToDecimal(value));
+                        break;
+                }
+                break;
+            case R.id.radioToBase8:
+                switch (from){
+                    case R.id.radioFromBase2:
+                        printResult = convert.decimalToOctal(convert.binaryToDecimal(Long.parseLong(value)));
+                        break;
+                    case R.id.radioFromBase8:
+                        printResult = value;
+                        break;
+                    case R.id.radioFromBase10:
+                        printResult = convert.decimalToOctal(Long.parseLong(value));
+                        break;
+                    case R.id.radioFromBase16:
+                        printResult = convert.decimalToOctal(convert.hexToDecimal(value));
+                        break;
+                }
+                break;
+            case R.id.radioToBase10:
+                switch (from){
+                    case R.id.radioFromBase2:
+                        printResult = String.valueOf(convert.binaryToDecimal(Long.parseLong(value)));
+                        break;
+                    case R.id.radioFromBase8:
+                        printResult = String.valueOf(convert.octalToDecimal(Long.parseLong(value)));
+                        break;
+                    case R.id.radioFromBase10:
+                        printResult = value;
+                        break;
+                    case R.id.radioFromBase16:
+                        printResult = String.valueOf(convert.hexToDecimal(value));
+                        break;
+                }
+                break;
+            case R.id.radioToBase16:
+                switch (from){
+                    case R.id.radioFromBase2:
+                        printResult = convert.decimalToHex(convert.binaryToDecimal(Long.parseLong(value)));
+                        break;
+                    case R.id.radioFromBase8:
+                        printResult = convert.decimalToHex(convert.octalToDecimal(Long.parseLong(value)));
+                        break;
+                    case R.id.radioFromBase10:
+                        printResult = convert.decimalToHex(Long.parseLong(value));
+                        break;
+                    case R.id.radioFromBase16:
+                        printResult = value;
+                        break;
+                }
+                break;
+        }
+        return printResult;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+
+
 
 }
